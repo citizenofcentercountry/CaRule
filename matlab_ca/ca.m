f2cb4cb05130e05e85420ea95f0a55da58404619
zhaoyun <zhaoyun@zpmc.com>
2018-01-25 16:22:03 +0800

    增加边的x和y轴字段

--- matlab_ca/ca.m
+++ matlab_ca/ca.m
@@ -99,6 +99,9 @@ axis equal
 axis tight
 axis([0,n,0,n]);
 axis xy;
+%title('Title','FontName','Times New Roman','FontWeight','Bold','FontSize',16)
+xlabel('x','FontName','Times New Roman','FontSize',14)
+ylabel('y','FontName','Times New Roman','FontSize',14,'Rotation',90)
 
 rng('shuffle');
 
