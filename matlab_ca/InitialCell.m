4022ab3c32c6457abfdcea7938027d67275d7e19
zhaoyun <zhaoyun@zpmc.com>
2018-05-17 14:32:28 +0800

    初始化部分进行随机化分布

--- matlab_ca/InitialCell.m
+++ matlab_ca/InitialCell.m
@@ -8,24 +8,45 @@ global st_i;
     disp(strcat('I number=', num2str(i_num)));
     disp(strcat('S number=', num2str(s_num)));
     disp(strcat('line number=', num2str(n)));
-%     %全部设置为正常状态
-%     for x = 1:n
-%         for y = 1:n
+    %全部设置为空白状态
+    for x = 1:n
+        for y = 1:n
+            cells(x,y) = st_n;
+        end
+    end
+
+%     %间隔1个为正常状态
+%     for x = 2:2:n-1
+%         for y = 2:2:n-1
 %             cells(x,y) = st_s;
 %         end
 %     end
 
-    %间隔1个为正常状态
-    for x = 2:2:n-1
-        for y = 2:2:n-1
-            cells(x,y) = st_s;
+    %随机设置易感节点的位置[2,n-1]*[2,n-1]
+    while s_num > 0
+        xy=ceil(rand(1,2) * n);
+        if xy(1,1) == 1 &&( xy(1,2)==1 || xy(1,2)==n)
+            continue;
+        end
+        if xy(1,1) == n &&( xy(1,2)==1 || xy(1,2)==n)
+            continue;
+        end
+        if (cells(xy(1,1), xy(1,2)) == st_n) 
+            cells(xy(1,1), xy(1,2)) = st_s;
+            s_num = s_num  - 1;
         end
     end
-
+    
     %随机设置感染节点的位置
     while i_num > 0
         xy=ceil(rand(1,2) * n);
-        if (cells(xy(1,1), xy(1,2)) ~= st_i) 
+        if xy(1,1) == 1 &&( xy(1,2)==1 || xy(1,2)==n)
+            continue;
+        end
+        if xy(1,1) == n &&( xy(1,2)==1 || xy(1,2)==n)
+            continue;
+        end
+        if (cells(xy(1,1), xy(1,2)) == st_n) 
             cells(xy(1,1), xy(1,2)) = st_i;
             i_num = i_num  - 1;
         end
